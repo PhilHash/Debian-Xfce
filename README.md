e5b0fb1781a6b07e7738176380cd6374ae7692e8
PhilHash <ht.phi99@gmail.com>
2024-07-23 23:20:21 +0700

    xfce install script

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # My-own-custom-debian-xfce-install
-This is my own way to install debian xfce 
+This is my own way to install debian xfce on laptop
+##
+First you can install a minimum install of debian with only the option 
---
+++ install.sh
@@ -0,0 +1,18 @@
+##my custom xfce on debian
+##check if you run script with sudo
+if ! [ $(id -u) = 0 ]; then
+  echo "This script must run with sudo, try again..."
+  exit 1
+fi
+
+
+sudo apt install -y libxfce4ui-utils thunar xfce4-panel gnome-themes-extra \
+xfce4-pulseaudio-plugin blueman gnome-disk-utility xfce4-session xfce4-settings \
+fonts-recommended xfconf xfdesktop4 xfwm4 thunar-volman xfce4-notifyd \
+xfce4-power-manager mousepad ristretto thunar-archive-plugin parole atril \
+xfce4-clipman-plugin ibus-unikey gammastep timeshift git gh nodejs podman \
+xfce4-screenshooter xfce4-taskmanager xfce4-terminal xfce4-whiskermenu-plugin \
+thunderbird flatpak gstreamer1.0-vaapi ffmpeg intel-media-va-driver-non-free \
+network-manager-gnome libreoffice-calc libreoffice-writer libreoffice-impress \
+libreoffice-gnome network-manager-openvpn-gnome firewall-config menulibre \
+libavcodec-extra slick-greeter ttf-mscorefonts-installer unrar
