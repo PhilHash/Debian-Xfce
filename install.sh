bacae19138edb99db991e11f5914a55f1384fbee
PhilHash <ht.phi99@gmail.com>
2024-07-29 08:49:58 +0700

    Change parole to vlc

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 sudo apt install -y libxfce4ui-utils thunar xfce4-panel gnome-themes-extra \
 	xfce4-pulseaudio-plugin blueman gnome-disk-utility xfce4-session xfce4-settings \
 	fonts-recommended xfconf xfdesktop4 xfwm4 thunar-volman xfce4-notifyd \
-	xfce4-power-manager mousepad ristretto thunar-archive-plugin parole atril \
+	xfce4-power-manager mousepad ristretto thunar-archive-plugin vlc atril \
 	xfce4-clipman-plugin ibus-unikey gammastep timeshift git gh nodejs podman \
 	xfce4-screenshooter xfce4-taskmanager xfce4-terminal xfce4-whiskermenu-plugin \
 	thunderbird flatpak gstreamer1.0-vaapi ffmpeg intel-media-va-driver-non-free \
